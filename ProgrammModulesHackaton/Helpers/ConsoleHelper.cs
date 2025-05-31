7c58794d7d745513ba67a5781613599de9c0a9e4
vovisim_home <wova53209@yandex.ru>
2025-05-31 18:32:47 +0300

    Обновил helpers

--- ProgrammModulesHackaton/Helpers/ConsoleHelper.cs
+++ ProgrammModulesHackaton/Helpers/ConsoleHelper.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace ProgrammModulesHackaton.Helpers
 {
-    internal class ConsoleHelper
+    public static class ConsoleHelper
     {
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKey key;
+
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    Console.Write("\b \b"); // удаляет последний символ на экране
+                    password = password[0..^1];
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write("*");
+                    password += keyInfo.KeyChar;
+                }
+
+            } while (key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            return password;
+        }
     }
 }
---
+++ ProgrammModulesHackaton/Helpers/PasswordHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace ProgrammModulesHackaton.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(password);
+            var hash = sha256.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            var hashOfInput = HashPassword(password);
+            return hashOfInput == hashedPassword;
+        }
+    }
+}
