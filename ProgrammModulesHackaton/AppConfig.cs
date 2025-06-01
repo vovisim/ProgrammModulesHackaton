488db505ddec792a0e6c822e2672a8d0b00215ac
vovisim_home <wova53209@yandex.ru>
2025-06-01 16:29:19 +0300

    Добавил конфиг

---
+++ ProgrammModulesHackaton/AppConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammModulesHackaton
+{
+    public static class AppConfig
+    {
+        public static readonly string ProjectDir =
+            Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName!;
+
+        public static readonly string DataDir = Path.Combine(ProjectDir, "Data");
+
+        public static readonly string DbPath = Path.Combine(DataDir, "database.db");
+
+        public static readonly string ConnectionString = $"Data Source={DbPath};";
+    }
+}
